a1fe2a759a0e9b162452e4598ddf179dba0685d9
ajayraj94 <73620896+ajayraj94@users.noreply.github.com>
2026-05-17 17:57:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -285,7 +285,7 @@ def listener():
 
                         elif text.startswith("/stop"):
                             stop_session = True
-                            send_msg(chat_id, "🛑 *Stopping quiz...* Kya tum gunda banega re, chhota quiz me stop stop khel rha hai.")
+                            send_msg(chat_id, "🛑 *Stopping quiz...*  leaderboard Generating....")
 
                         elif text.startswith("/stats") or text.startswith("/leaderboard"):
                             show_leaderboard(chat_id)
